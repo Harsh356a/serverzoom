7aa1bab8f61a2d3574585740545d27fb9cba5c2d
coderHarsh121212 <codesource287@gmail.com>
2024-09-02 00:28:31 +0530

    harsh

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3001;
 const path = require('path');
-
+const cors = require('cors');
 let socketList = {};
+let rooms = {};
 
-// app.use(express.static(path.join(__dirname, 'public'))); // this will work for CRA build
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../vite')));
-
+app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -20,58 +21,56 @@ if (process.env.NODE_ENV === 'production') {
 
 // Route
 app.get('/ping', (req, res) => {
-  res
-    .send({
-      success: true,
-    })
-    .status(200);
+  res.status(200).send({ success: true });
 });
+
+function cleanupRoom(roomId) {
+  if (rooms[roomId] && rooms[roomId].size === 0) {
+    delete rooms[roomId];
+    console.log(`Room ${roomId} has been deleted due to inactivity`);
+  }
+}
 
 // Socket
 io.on('connection', (socket) => {
   console.log(`New User connected: ${socket.id}`);
 
   socket.on('disconnect', () => {
-    socket.disconnect();
+    delete socketList[socket.id];
     console.log('User disconnected!');
   });
 
   socket.on('BE-check-user', ({ roomId, userName }) => {
     let error = false;
     console.log('BE-check-user', roomId, userName);
-    io.sockets.in(roomId).clients((err, clients) => {
-      clients.forEach((client) => {
-        if (socketList[client] == userName) {
-          error = true;
-        }
-      });
-      socket.emit('FE-error-user-exist', { error });
-    });
+    
+    if (rooms[roomId] && rooms[roomId].has(userName)) {
+      error = true;
+    }
+    
+    socket.emit('FE-error-user-exist', { error });
   });
 
-  /**
-   * Join Room
-   */
   socket.on('BE-join-room', ({ roomId, userName }) => {
-    // Socket Join RoomName
     console.log('BE-join-room', roomId, userName);
     socket.join(roomId);
     socketList[socket.id] = { userName, video: true, audio: true };
 
-    // Set User List
-    io.sockets.in(roomId).clients((err, clients) => {
-      try {
-        const users = [];
-        clients.forEach((client) => {
-          // Add User List
-          users.push({ userId: client, info: socketList[client] });
-        });
-        socket.broadcast.to(roomId).emit('FE-user-join', users);
-        // io.sockets.in(roomId).emit('FE-user-join', users);
-      } catch (e) {
-        io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
+    if (!rooms[roomId]) {
+      rooms[roomId] = new Set();
+    }
+    rooms[roomId].add(userName);
+
+    const users = Array.from(rooms[roomId]).map(user => ({
+      userId: Object.keys(socketList).find(id => socketList[id].userName === user) || null,
+      info: {
+        userName: user,
+        video: socketList[Object.keys(socketList).find(id => socketList[id].userName === user)]?.video || true,
+        audio: socketList[Object.keys(socketList).find(id => socketList[id].userName === user)]?.audio || true
       }
-    });
+    }));
+
+    socket.broadcast.to(roomId).emit('FE-user-join', users);
   });
 
   socket.on('BE-call-user', ({ userToCall, from, signal }) => {
@@ -95,24 +94,82 @@ io.on('connection', (socket) => {
 
   socket.on('BE-leave-room', ({ roomId, leaver }) => {
     delete socketList[socket.id];
-    socket.broadcast
-      .to(roomId)
-      .emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
-    io.sockets.sockets[socket.id].leave(roomId);
+    socket.broadcast.to(roomId).emit('FE-user-leave', { userName: leaver });
+    socket.leave(roomId);
+    if (rooms[roomId]) {
+      rooms[roomId].delete(leaver);
+      cleanupRoom(roomId);
+    }
   });
 
   socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
-    if (switchTarget === 'video') {
-      socketList[socket.id].video = !socketList[socket.id].video;
+    if (socketList[socket.id]) {
+      if (switchTarget === 'video') {
+        socketList[socket.id].video = !socketList[socket.id].video;
+      } else {
+        socketList[socket.id].audio = !socketList[socket.id].audio;
+      }
+      socket.broadcast
+        .to(roomId)
+        .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
     } else {
-      socketList[socket.id].audio = !socketList[socket.id].audio;
+      console.log(`Socket ${socket.id} not found in socketList`);
+      socket.emit('FE-error', { message: 'User not found in room' });
     }
-    socket.broadcast
-      .to(roomId)
-      .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
   });
 });
 
+app.post('/api/addUser', (req, res) => {
+  const { roomId, userName } = req.body;
+  
+  if (!roomId || !userName) {
+    return res.status(400).json({ error: 'roomId and userName are required' });
+  }
+
+  if (!rooms[roomId]) {
+    rooms[roomId] = new Set();
+  }
+
+  if (rooms[roomId].has(userName)) {
+    return res.status(409).json({ error: 'User already exists in the room' });
+  }
+
+  rooms[roomId].add(userName);
+  
+  // Notify all clients in the room about the new user
+  io.to(roomId).emit('FE-user-join', [{
+    userId: null,
+    info: {
+      userName: userName,
+      video: true,
+      audio: true
+    }
+  }]);
+
+  res.status(200).json({ message: 'User added successfully' });
+});
+
+app.post('/api/removeUser', (req, res) => {
+  const { roomId, userName } = req.body;
+  
+  if (!roomId || !userName) {
+    return res.status(400).json({ error: 'roomId and userName are required' });
+  }
+
+  if (!rooms[roomId] || !rooms[roomId].has(userName)) {
+    return res.status(404).json({ error: 'User not found in the room' });
+  }
+
+  rooms[roomId].delete(userName);
+  
+  // Notify all clients in the room about the user leaving
+  io.to(roomId).emit('FE-user-leave', { userName });
+
+  cleanupRoom(roomId);
+
+  res.status(200).json({ message: 'User removed successfully' });
+});
+
 http.listen(PORT, () => {
-  console.log('Connected : 3001');
+  console.log(`Server is running on port ${PORT}`);
 });
