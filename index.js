fc1e2e7f25a0e3869b5b9ff2d3d58efbc004195f
coderHarsh121212 <codesource287@gmail.com>
2024-09-03 15:42:39 +0530

    harsh

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let rooms = {};
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../vite")));
-app.use(cors());
+app.use(cors("*"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
